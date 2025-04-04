31db7a5a97202d545d26225b2498bebbfc528c4f
WinashAntsaw <winashantsaw4@gmail.com>
2025-04-04 18:54:17 -0400

    added skeleton to script.js and readded data files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-"//js" 
+const UNFILTERED_PATH = "../data/greenhouse_unfiltered.csv"
+
+
+function init() {
+    d3.csv(UNFILTERED_PATH).then(data => {
+        console.log(data);
+        createWhiteHat(data);
+        createBlackHat(data);
+    })
+    .catch(error => console.error('Error loading data:', error));
+}
+
+
+
+function createWhiteHat(data) {
+
+}
+
+function createBlackHat(data) {
+
+}
+
+window.addEventListener('load', init)
