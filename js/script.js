77eb610c9189467a37179ff84afe9c9e24d18736
WinashAntsaw <winashantsaw4@gmail.com>
2025-04-04 19:50:57 -0400

    added some data filtering

--- js/script.js
+++ js/script.js
@@ -1,15 +1,54 @@
-const UNFILTERED_PATH = "../data/greenhouse_unfiltered.csv"
-
+const UNFILTERED_PATH = "./data/greenhouse_unfiltered.csv"
+/*
+ACTION
+BASE_PER
+Base period
+CONVERSION_TYPE
+Conversion type
+DECIMALS
+Decimals
+FREQ
+Frequency of observation
+MEASURE
+Measure
+OBS_STATUS
+OBS_VALUE
+Observation status
+Observation value
+POLLUTANT
+PRICE_BASE
+Pollutant
+Price base
+REF_AREA
+Reference area
+STRUCTURE
+STRUCTURE_ID
+STRUCTURE_NAME
+TIME_PERIOD
+Time period
+UNIT_MEASURE
+UNIT_MULT
+Unit multiplier
+Unit of measure
+*/
 
 function init() {
     d3.csv(UNFILTERED_PATH).then(data => {
-        console.log(data);
+        cleanUpData(data);
+        console.log(data[0]);
         createWhiteHat(data);
         createBlackHat(data);
     })
     .catch(error => console.error('Error loading data:', error));
 }
 
+function cleanUpData(data){
+    data = data.forEach(function(d) {
+        delete d.STRUCTURE_NAME;
+        delete d.STRUCTURE_ID;
+        delete d.STRUCTURE;
+    })
+}
 
 
 function createWhiteHat(data) {
